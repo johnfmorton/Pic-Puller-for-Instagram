fd7c4a82337158b8059e25216855d31be2f34b5b
John Morton <john@johnfmorton.com>
2012-01-20 11:33:46 -0500

    Adding config file to support NSM Addon Updater

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+$config['name']='Pic Puller for Instagram';
+$config['version']='0.8.0';
+$config['nsm_addon_updater']['versions_xml']='http://static.supergeekery.com/nsm_addon_updater/changelog_picpuller_ig.xml';
