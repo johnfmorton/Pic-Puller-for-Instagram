0c2e5a41bcb6775acafca059d6bcadd704cdcf0e
John Morton <john@johnfmorton.com>
2012-01-24 18:37:53 -0500

    Updated config file to v1

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
 $config['name']='Pic Puller for Instagram';
-$config['version']='0.8.0';
+$config['version']='1.0.0';
 $config['nsm_addon_updater']['versions_xml']='http://static.supergeekery.com/nsm_addon_updater/changelog_picpuller_ig.xml';
