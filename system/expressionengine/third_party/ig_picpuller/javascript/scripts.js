4214f964557c5e23fbfa7b5d5aa0b8e94aacd79b
John Morton <john@johnfmorton.com>
2012-02-28 19:27:23 -0500

    Updated scripts file for Matrix compatibility

--- system/expressionengine/third_party/ig_picpuller/javascript/scripts.js
+++ system/expressionengine/third_party/ig_picpuller/javascript/scripts.js
@@ -4,6 +4,32 @@ $(function() {
 	 // The Field Type's photo viewer won't work properly without JS, so it's hidden until the JS has loaded.
 
 	$('.igbrowserbt').show();
- 	$('.igbrowserbt').colorbox({width:"830px", height:"515px", title: 'Choose a photo from your Instagram feed'});
+ 	$('.igbrowserbt').colorbox({width:"830px", height:"515px", title: 'Choose a photo from your Instagram feed', 
+ 				onOpen: function() {
+ 						//$(this).parent().$('input').attr('id', 'activePPtarget');
+ 						$(this).parent().find('input').attr('id', 'activePPtarget');
+ 						//console.log($(this).parent().find('input'));
+ 					},
+ 				onCleanup: function() {
+ 						$('#activePPtarget').removeAttr('id');
+ 					} });
+
+
+ 	Matrix.bind('ig_picpuller', 'display', function(cell){
+		// Upon the display of each new PP browser row within a Matrix field, this JS is fired
+		$('.igbrowserbtmatrix').show();
+ 		$('.igbrowserbtmatrix').colorbox({
+ 				width:"830px", 
+ 				height:"515px", 
+ 				title: 'Choose a photo from your Instagram feed', 
+ 				onOpen: function() {
+ 						//$(this).prev().attr('id', 'activePPtarget');
+ 						$(this).parent().find('input').attr('id', 'activePPtarget');
+ 					},
+ 				onCleanup: function() {
+ 						$('#activePPtarget').removeAttr('id');
+ 					} 
+ 				});
+	});
 });
 
